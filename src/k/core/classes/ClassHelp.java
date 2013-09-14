0584c52754f6cd369fc823da43549d0fa78fbf1a
Kenzie Togami <ket1999@gmail.com>
2013-09-13 21:30:37 -0700

    Added low-level types to ClassHelp.castable()

--- src/k/core/classes/ClassHelp.java
+++ src/k/core/classes/ClassHelp.java
@@ -2,8 +2,22 @@ package k.core.classes;
 
 public class ClassHelp {
 	public static boolean castable(Class<?> a, Class<?> b) {
-		return a.isAssignableFrom(b) || (a == int.class && b == Integer.class)
-				|| (a == Integer.class && b == int.class);
+		return a.isAssignableFrom(b) || (a == int.class && b == Integer.class) // int
+				|| (a == Integer.class && b == int.class) // int
+				|| (a == Short.class && b == short.class) // short
+				|| (a == short.class && b == Short.class) // short
+				|| (a == Byte.class && b == byte.class) // byte
+				|| (a == byte.class && b == Byte.class) // byte
+				|| (a == Long.class && b == long.class) // long
+				|| (a == long.class && b == Long.class) // long
+				|| (a == Float.class && b == float.class) // float
+				|| (a == float.class && b == Float.class) // float
+				|| (a == Double.class && b == double.class) // double
+				|| (a == double.class && b == Double.class) // double
+				|| (a == Character.class && b == char.class) // char
+				|| (a == char.class && b == Character.class) // char
+				|| (a == Boolean.class && b == boolean.class) // boolean
+				|| (a == boolean.class && b == Boolean.class); // boolean
 	}
 
 	public static Class<?>[] classesFromObjects(Object[] objects) {
