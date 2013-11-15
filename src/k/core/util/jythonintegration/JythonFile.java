8c4e0e90986c3ca138737b337b85e2800f33bff4
Kenzie Togami <ket1999@gmail.com>
2013-11-14 18:37:43 -0800

    jython file methods now accessible

--- src/k/core/util/jythonintegration/JythonFile.java
+++ src/k/core/util/jythonintegration/JythonFile.java
@@ -1,39 +1,49 @@
 package k.core.util.jythonintegration;
 
 import java.io.File;
+import java.rmi.UnexpectedException;
 
 import org.python.core.PyMethod;
 import org.python.core.PyObject;
+import org.python.core.PyType;
 import org.python.util.PythonInterpreter;
 
 public class JythonFile {
 	private File f = null;
-	
+
 	/**
 	 * Holds the private interpreter, if one is needed
 	 */
 	private PythonInterpreter interpreter = null;
-	
+
 	public JythonFile(File pyfile, boolean newInterpreter) {
 		f = pyfile.getAbsoluteFile();
-		if(newInterpreter) {
+		if (newInterpreter) {
 			interpreter = new PythonInterpreter();
 		} else {
 			interpreter = JythonIntergration.mainInterpreter;
 		}
 		interpreter.execfile(f.getAbsolutePath());
 	}
-	
+
 	/**
 	 * Invokes the given method
-	 * @param mName - the name of the moethod to invoke
-	 * @param args - 
+	 * 
+	 * @param mName
+	 *            - the name of the moethod to invoke
+	 * @param args
+	 *            -
 	 * @return
 	 */
 	public PyObject invokeMethod(String mName, PyObject... args) {
 		PyObject invokeres = null;
 		PyObject method = interpreter.get(mName);
-		invokeres = method.__call__();
+		if (method.getType().getName().equals("function")) {
+			invokeres = method.__call__();
+		} else {
+			throw new RuntimeException(new UnexpectedException(
+					"Expected function, got " + method.getType().getName()));
+		}
 		return invokeres;
 	}
 }
