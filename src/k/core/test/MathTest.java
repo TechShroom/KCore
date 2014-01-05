5548e3671189287d7f61e4eceb2f396434a5acf3
Kenzie Togami <ket1999@gmail.com>
2014-01-06 08:04:51 +1100

    More specific test or UDs

--- src/k/core/test/MathTest.java
+++ src/k/core/test/MathTest.java
@@ -9,22 +9,38 @@ public class MathTest {
      */
     public static void main(String[] args) {
         UnlimitedDouble a = new UnlimitedDouble("4");
-        UnlimitedDouble b = new UnlimitedDouble("60");
+        System.err.println(a);
+        UnlimitedDouble b = new UnlimitedDouble("60.0");
+        System.err.println(b);
         UnlimitedDouble c = a.multiply(b); // a * b test
-        System.err.println("UD c = " + c);
+        System.err.println("UD c = " + c + ", should be " + 4 * 60);
 
         c = a.add(b); // a + b test
-        System.err.println("UD c = " + c);
-
-        System.err.println("\n\n\n\n");
+        System.err.println("UD c = " + c + ", should be " + (4 + 60));
 
         // invalid tests
-        c = new UnlimitedDouble("!");
-        c = new UnlimitedDouble("-1");
-        c = new UnlimitedDouble("1.00.02");
+        try {
+            c = new UnlimitedDouble("!");
+            System.err.println(c);
+        } catch (NumberFormatException nfe) {
+            System.err.println("invalid (this should happen!)");
+        }
+        try {
+            c = new UnlimitedDouble("-1");
+            System.err.println(c);
+        } catch (NumberFormatException nfe) {
+            System.err.println("invalid (this shouldn't happen!)");
+        }
+        try {
+            c = new UnlimitedDouble("1.00.02");
+            System.err.println(c);
+        } catch (NumberFormatException nfe) {
+            System.err.println("invalid (this should happen!)");
+        }
 
         // test to ensure valid things are not marked as invalid
-        c = new UnlimitedDouble("1.0003");
+        c = new UnlimitedDouble("-001.0003");
+        System.err.println(c);
     }
 
 }
