63eaf0226847be829f961bd56c718b25e930e92c
Kenzie Togami <ket1999@gmail.com>
2014-01-06 10:23:10 +1100

    Move tests to methods

--- src/k/core/test/MathTest.java
+++ src/k/core/test/MathTest.java
@@ -4,39 +4,54 @@ import k.core.util.math.UnlimitedDouble;
 
 public class MathTest {
     public static void main(String[] args) {
-        UnlimitedDouble a = new UnlimitedDouble("4");
+        UnlimitedDouble a = new UnlimitedDouble("4"), b = new UnlimitedDouble(
+                "60.0"), c = UnlimitedDouble.empty();
         System.err.println(a);
-        UnlimitedDouble b = new UnlimitedDouble("60.0");
         System.err.println(b);
+        add(a, b);
+        multiply(a, b);
+
+        invalid();
+
+        // test to ensure valid things are not marked as invalid
+        c = new UnlimitedDouble("-001.0003");
+        System.err.println(c);
+    }
+
+    public static void multiply(UnlimitedDouble a, UnlimitedDouble b) {
         UnlimitedDouble c = a.multiply(b); // a * b test
-        System.err.println("UD c = " + c + ", should be " + 4 * 60);
+        System.err.println("UD c = " + c);
+    }
 
-        c = a.add(b); // a + b test
-        System.err.println("UD c = " + c + ", should be " + (4 + 60));
+    public static void add(UnlimitedDouble a, UnlimitedDouble b) {
+        UnlimitedDouble c = a.add(b); // a + b test
+        System.err.println("UD c = " + c);
+    }
 
+    public static void invalid() {
+        UnlimitedDouble c = UnlimitedDouble.empty();
         // invalid tests
         try {
             c = new UnlimitedDouble("!");
             System.err.println(c);
         } catch (NumberFormatException nfe) {
             System.err.println("invalid (this should happen!)");
+            nfe.printStackTrace();
         }
         try {
             c = new UnlimitedDouble("-1");
             System.err.println(c);
         } catch (NumberFormatException nfe) {
             System.err.println("invalid (this shouldn't happen!)");
+            nfe.printStackTrace();
         }
         try {
             c = new UnlimitedDouble("1.00.02");
             System.err.println(c);
         } catch (NumberFormatException nfe) {
             System.err.println("invalid (this should happen!)");
+            nfe.printStackTrace();
         }
-
-        // test to ensure valid things are not marked as invalid
-        c = new UnlimitedDouble("-001.0003");
-        System.err.println(c);
     }
 
 }
