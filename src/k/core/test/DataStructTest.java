a868e46c9cb09eeeec14edc094588fd2603c9a8c
Kenzie Togami <ket1999@gmail.com>
2014-01-11 15:55:16 -0800

    Add DataStruct

---
+++ src/k/core/test/DataStructTest.java
@@ -0,0 +1,27 @@
+package k.core.test;
+
+import k.core.util.Helper;
+import k.core.util.netty.DataStruct;
+
+public class DataStructTest {
+    String aVal = "'look, it worked, alright?' -" + getClass().getSimpleName();
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Object[] all = new Object[] { "string", 1, 1.1d, 1.1f, 1l, (byte) 1,
+                '|', true, new DataStructTest() };
+        Helper.BetterArrays.print(all);
+        // A data struct with all types in one.
+        DataStruct dec = new DataStruct(all);
+        DataStruct enc = new DataStruct(dec.toString());
+        Helper.BetterArrays.print(enc.getAll());
+    }
+
+    @Override
+    public String toString() {
+        return aVal;
+    }
+
+}
