623bf2c8e9670b36ac63072068d43ff259716276
Kenzie Togami <ket1999@gmail.com>
2014-01-11 16:28:49 -0800

    Add test for LRFPS

---
+++ src/k/core/test/LRFPSTest.java
@@ -0,0 +1,58 @@
+package k.core.test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import k.core.util.timing.LowResFPS;
+
+public class LRFPSTest {
+    static JFrame frame = new JFrame("title");
+
+    public static void main(String[] args) throws InvocationTargetException,
+	    InterruptedException {
+	frame.setSize(200, 100);
+	frame.setLocation(400, 600);
+	frame.setVisible(true);
+	loop();
+    }
+
+    private static void loop() throws InvocationTargetException,
+	    InterruptedException {
+	final int index = LowResFPS.genIndex();
+	LowResFPS.init(index);
+	while (true) {
+	    int delta = LowResFPS.update(index);
+	    System.err.println(delta);
+	    do_complicated_logic();
+	    SwingUtilities.invokeAndWait(new Runnable() {
+
+		@Override
+		public void run() {
+		    frame.setTitle("FPS: "
+			    + String.valueOf(LowResFPS.getFPS(index)));
+		}
+	    });
+	}
+    }
+
+    private static void do_complicated_logic() {
+	for (int i = 0; i < 123; i++) {
+	    dont_burn_the_processor();
+	}
+    }
+
+    private static void dont_burn_the_processor() {
+	try {
+	    int b00m = Integer.parseInt("B00M", Character.MAX_RADIX);
+	    try {
+		Thread.sleep((long) (b00m / 500000.0));
+	    } catch (InterruptedException boom) {
+		throw new RuntimeException("boom!", boom);
+	    }
+	} catch (NumberFormatException e) {
+	    throw new RuntimeException(e);
+	}
+    }
+}
