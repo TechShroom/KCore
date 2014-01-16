2c90fb7e8eb8a669bcfc0ff2513737141a3f2cbc
Kenzie Togami <ket1999@gmail.com>
2014-01-16 09:37:32 -0800

    Arduino API Basics, NYI

---
+++ src/k/core/util/arduino/ArduinoInputStream.java
@@ -0,0 +1,13 @@
+package k.core.util.arduino;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ArduinoInputStream extends InputStream {
+
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
+
+}
---
+++ src/k/core/util/arduino/ArduinoOutputStream.java
@@ -0,0 +1,12 @@
+package k.core.util.arduino;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ArduinoOutputStream extends OutputStream {
+
+    @Override
+    public void write(int b) throws IOException {
+    }
+
+}
