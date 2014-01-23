19bdfa3967b277c9aad8e47a28815f04bd9f8b25
Kenzie Togami <ket1999@gmail.com>
2014-01-23 07:26:46 -0800

    Add tester. Launch it with some arguments.

---
+++ src/k/core/util/PPTest.java
@@ -0,0 +1,16 @@
+package k.core.util;
+
+import java.util.Arrays;
+
+import k.core.util.Helper.ProgramProps;
+
+public class PPTest {
+
+    public static void main(String[] args) {
+        System.err.println(Arrays.toString(args));
+        args = ProgramProps.normalizeCommandArgs(args);
+        ProgramProps.acceptAll(args);
+        System.err.println(Arrays.toString(args));
+    }
+
+}
