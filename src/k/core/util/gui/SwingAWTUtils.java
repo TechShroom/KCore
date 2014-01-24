0a86ad8b0a3ee52b4e413cdf85acef8770e8f426
Kenzie Togami <ket1999@gmail.com>
2014-01-23 17:06:57 -0800

    Add some helper Swing methods

--- src/k/core/util/gui/SwingAWTUtils.java
+++ src/k/core/util/gui/SwingAWTUtils.java
@@ -1,11 +1,15 @@
 package k.core.util.gui;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Toolkit;
 import java.awt.event.WindowEvent;
+import java.util.Arrays;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class SwingAWTUtils {
@@ -33,5 +37,52 @@ public class SwingAWTUtils {
         } else {
             SwingUtilities.invokeAndWait(r);
         }
+    }
+
+    public static void removeAll(JComponent c) {
+        c.removeAll();
+        c.validate();
+        c.repaint();
+    }
+
+    /**
+     * Adds all of the specified {@link Component components} with the given
+     * constraints, then calls {@link SwingAWTUtils#validate(JPanel) validate()}
+     * on <tt>to</tt>.
+     * 
+     * @param to
+     *            - the {@link JPanel} to add to
+     * @param comps
+     *            - the components to add
+     * @param constraints
+     *            - the constraints. If <tt>null</tt>, a new array of size
+     *            <tt>comps.length</tt> is created.
+     * @see SwingAWTUtils#validate(JPanel)
+     */
+    public static void addAllAndValidate(JPanel to, Component[] comps,
+            Object[] constraints) {
+        if (constraints == null) {
+            constraints = new Object[comps.length];
+        } else if (constraints.length < comps.length) {
+            constraints = Arrays.copyOf(constraints, comps.length);
+        }
+        for (int i = 0; i < comps.length; i++) {
+            to.add(comps[i], constraints[i]);
+        }
+        validate(to);
+    }
+
+    /**
+     * Calls {@link JPanel#validate()} then {@link JPanel#repaint()} on the
+     * given {@link JPanel} to fully reload the JPanel.
+     * 
+     * @param jp
+     *            - the JPanel to use
+     * @see JPanel#repaint()
+     * @see JPanel#validate()
+     */
+    public static void validate(JPanel jp) {
+        jp.validate();
+        jp.repaint();
     }
 }
