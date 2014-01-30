1f64495d3d9c71c521e3efd2eb4d7e58de34069b
Kenzie Togami <ket1999@gmail.com>
2014-01-30 12:14:16 -0800

    Don't use sync buffer

--- src/k/core/util/streams/InputPipeStream.java
+++ src/k/core/util/streams/InputPipeStream.java
@@ -2,10 +2,57 @@ package k.core.util.streams;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class InputPipeStream extends InputStream {
+    private static class PipeRunnable implements Runnable {
+        private PipedInputStream in = null;
+        private AtomicIntegerArray aia = new AtomicIntegerArray(4096);
+        private AtomicInteger reqdbytes = new AtomicInteger(-1);
 
-    private SyncBuffer buf = null;
+        public PipeRunnable(PipedInputStream i) {
+            in = i;
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                if (reqdbytes.get() > 0) {
+                    for (int i = 0; i < reqdbytes.get(); i++) {
+                        try {
+                            aia.addAndGet(i, in.read());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                            break;
+                        }
+                    }
+                    if (Thread.interrupted()) {
+                        break;
+                    }
+                    reqdbytes.set(-1);
+                }
+            }
+        }
+
+        public int read() {
+            reqdbytes.set(1);
+            while (reqdbytes.get() > 0) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ie) {
+                }
+            }
+            return aia.get(0);
+        }
+    }
+
+    private PipeRunnable running = null;
+    private PipedInputStream i = new PipedInputStream();
+    private boolean conn = false;
+    private Thread runningt;
 
     public InputPipeStream() {
     }
@@ -14,21 +61,40 @@ public class InputPipeStream extends InputStream {
         if (connected() || ops.connected()) {
             throw new IllegalStateException("Already connected");
         }
-        buf = new SyncBuffer(new byte[4096]);
-        ops.setbuf(buf);
+        try {
+            i.connect(ops.getSrc());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        running = new PipeRunnable(i);
+        runningt = new Thread(running, "PipeIn");
+        runningt.setDaemon(true);
+        runningt.start();
+        conn = true;
     }
 
     public boolean connected() {
-        return buf != null;
+        return conn;
     }
 
     @Override
     public int read() throws IOException {
-        return buf.get();
+        return running.read();
     }
 
-    protected void setbuf(SyncBuffer b) {
-        buf = b;
+    PipedInputStream getSnk() {
+        return i;
+    }
+
+    @Override
+    public void close() {
+        try {
+            i.close();
+            runningt.interrupt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/k/core/util/streams/OutputPipeStream.java
+++ src/k/core/util/streams/OutputPipeStream.java
@@ -2,10 +2,52 @@ package k.core.util.streams;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class OutputPipeStream extends OutputStream {
+    private static class PipeRunnable implements Runnable {
+        private PipedOutputStream out = null;
+        private AtomicIntegerArray aia = new AtomicIntegerArray(4096);
+        private AtomicInteger reqdbytes = new AtomicInteger(-1);
 
-    private SyncBuffer buf = null;
+        public PipeRunnable(PipedOutputStream o) {
+            out = o;
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                if (reqdbytes.get() > 0) {
+                    for (int i = 0; i < reqdbytes.get(); i++) {
+                        try {
+                            out.write(aia.get(i));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                            break;
+                        }
+                    }
+                    if (Thread.interrupted()) {
+                        break;
+                    }
+                    reqdbytes.set(-1);
+                }
+            }
+        }
+
+        public void write(int b) {
+            aia.addAndGet(0, b);
+            reqdbytes.set(1);
+        }
+
+    }
+
+    private PipeRunnable running = null;
+    private PipedOutputStream o = new PipedOutputStream();
+    private boolean conn = false;
+    private Thread runningt;
 
     public OutputPipeStream() {
     }
@@ -14,24 +56,32 @@ public class OutputPipeStream extends OutputStream {
         if (connected() || ips.connected()) {
             throw new IllegalStateException("Already connected");
         }
-        buf = new SyncBuffer(new byte[4096]);
-        ips.setbuf(buf);
+        try {
+            o.connect(ips.getSnk());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        conn = true;
     }
 
     public boolean connected() {
-        return buf != null;
+        return conn;
     }
 
     @Override
     public void write(int b) throws IOException {
-        if (buf == null) {
-            throw new IllegalStateException("Not connected");
-        }
-        buf.add((byte) b);
+        running.write(b);
     }
 
-    protected void setbuf(SyncBuffer b) {
-        buf = b;
+    PipedOutputStream getSrc() {
+        return o;
     }
 
+    void setup() {
+        running = new PipeRunnable(o);
+        runningt = new Thread(running, "PipeOut");
+        runningt.setDaemon(true);
+        runningt.start();
+    }
 }
