8b2357532aa06ac744fee3d783f5f3bec802e743
Kenzie Togami <ket1999@gmail.com>
2014-02-06 08:09:50 -0800

    Fix blocking calls from super()

--- src/k/core/util/netty/Packet.java
+++ src/k/core/util/netty/Packet.java
@@ -39,6 +39,7 @@ public abstract class Packet implements Comparable<Packet> {
     static {
         registerPacket(PacketFinished.class, TERMINATION_PACKET_ID);
     }
+    private final String PACKET_CLASS_NAME = getClass().getName();
 
     /**
      * Uses an empty constructor for a packet.
@@ -118,8 +119,7 @@ public abstract class Packet implements Comparable<Packet> {
      * {@link Packet#newPacket(int, Class[], Object[])}
      */
     protected Packet(DataStruct ds) {
-        if (!StackTraceInfo.getInvokingClassName().equals(
-                Packet.class.getName())) {
+        if (!StackTraceInfo.getInvokingClassName().equals(PACKET_CLASS_NAME)) {
             throw new IllegalAccessError(
                     "cannot create Packet class via constructor");
         }
