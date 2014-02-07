dd1c899686efb8551670d4445a23bee73134e8e2
Kenzie Togami <ket1999@gmail.com>
2014-02-06 19:26:02 -0800

    Use debug in MathTest

--- src/k/core/util/math/MathTest.java
+++ src/k/core/util/math/MathTest.java
@@ -4,6 +4,7 @@ import static k.core.util.math.UnlimitedDouble.*;
 
 public class MathTest {
     public static void main(String[] args) {
+        System.setProperty("ud.debug", Boolean.TRUE.toString());
         UnlimitedDouble a = newInstance("4.010"), b = newInstance("60.0"), c = UnlimitedDouble
                 .empty();
         System.err.println(a);
