dbecd2c657ea6cac6d5ec73f0d778ad1383f0227
Kenzie Togami <ket1999@gmail.com>
2014-03-09 11:06:11 -0700

    Add json conversions

--- src/k/core/util/github/RateLimit.java
+++ src/k/core/util/github/RateLimit.java
@@ -3,6 +3,11 @@ package k.core.util.github;
 import java.util.Calendar;
 import java.util.Date;
 
+import k.core.util.github.gitjson.GithubJsonCreator;
+import k.core.util.github.gitjson.GithubJsonParser;
+
+import com.google.gson.JsonPrimitive;
+
 public final class RateLimit {
     public static enum RateType {
         CORE, SEARCH;
@@ -14,6 +19,7 @@ public final class RateLimit {
         staticCal.setTimeInMillis(millis);
         return staticCal.getTime();
     }
+
     private final int remaining, limit;
     private final long reset;
 
@@ -40,6 +46,22 @@ public final class RateLimit {
 
     public Date getResetTimeAsDate() {
         return resetDate;
+    }
+
+    public String json() {
+        return GithubJsonCreator.getForObjectCreation()
+                .add("limit", new JsonPrimitive(limit))
+                .add("reset", new JsonPrimitive(reset))
+                .add("remaining", new JsonPrimitive(remaining)).toString();
+    }
+
+    public static RateLimit fromJSON(String json) {
+        GithubJsonParser parser = GithubJsonParser.begin(json);
+        JsonPrimitive lim = parser.data("limit").getAsJsonPrimitive();
+        JsonPrimitive res = parser.data("reset").getAsJsonPrimitive();
+        JsonPrimitive rem = parser.data("remaining").getAsJsonPrimitive();
+        parser.end();
+        return new RateLimit(rem.getAsInt(), lim.getAsInt(), res.getAsLong());
     }
 
     @Override
